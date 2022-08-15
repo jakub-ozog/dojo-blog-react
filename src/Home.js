e008c97dadf61406c02cddf2a4e90b47943641b8
jacobharraldson <jharraldson@gmail.com>
2022-08-15 23:30:51 +0200

    Fetch data with JSON server/Custom Hooks/Router

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,15 @@
 import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        {title: 'Welcome Party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-    ]);
-
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-        console.log('use effect ran');
-    }, [])
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return  (
         <div className="home">
-           <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading..</div> }
+            { blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
 }
