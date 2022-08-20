1975f99f82fb4b866f3b9f9a20a30baf9b03c78b
jacobharraldson <jharraldson@gmail.com>
2022-08-20 21:38:03 +0200

    Fetch data with JSON server/Custom Hooks/React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import Navbar from "./Navbar";
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Create from './Create';
+import BlogDetails from './BlogDetails'
 
 function App() {
-  return (
-    <div className="App">
-          <Navbar />
-      <div className='content'>
-          <Home />
-
-      </div>
-
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <div className='content'>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>}>
+                        </Route>
+                    </Routes>
+                    <Routes>
+                        <Route path="/create" element={<Create/>}>
+                        </Route>
+                    </Routes>
+                    <Routes>
+                        <Route path="/blogs/:id" element={<BlogDetails/>}>
+                        </Route>
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/useFetch.js
+++ src/useFetch.js
@@ -8,8 +8,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     console.log(res)
                     if(!res.ok) {
@@ -23,10 +25,18 @@ const useFetch = (url) => {
                     setError(null)
                 })
                 .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted')
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                    }
                     setIsPending(false)
                     setError(err.message)
                 })
-        }, 1000);
+        }, 500);
+
+        return () => abortCont.abort();
     }, [url]);
 
 return { data, isPending, error }
